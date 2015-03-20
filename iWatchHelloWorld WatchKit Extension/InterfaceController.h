745ad7287cecf28514bc73433e5d4a899788b49d
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-03-20 19:11:11 +0530

    # Initial Commit

---
+++ iWatchHelloWorld WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  iWatchHelloWorld WatchKit Extension
+//
+//  Created by Vinod Vishwakarma on 20/03/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ iWatchHelloWorld WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  iWatchHelloWorld WatchKit Extension
+//
+//  Created by Vinod Vishwakarma on 20/03/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
