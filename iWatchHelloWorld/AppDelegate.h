fe173ebd6825f831f596638a732f7c4e96531614
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-03-20 19:02:18 +0530

    Initial Commit

---
+++ iWatchHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWatchHelloWorld
+//
+//  Created by Vinod Vishwakarma on 20/03/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
